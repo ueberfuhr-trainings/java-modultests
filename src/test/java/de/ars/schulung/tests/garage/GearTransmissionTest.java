90b3998b9bae4d9be17be53f310b3fda7d2cccbf
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2019-09-13 08:30:07 +0200

    Einfacher Test für GearTransmission ergänzt

---
+++ src/test/java/de/ars/schulung/tests/garage/GearTransmissionTest.java
@@ -0,0 +1,73 @@
+package de.ars.schulung.tests.garage;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+import de.ars.schulung.tests.garage.exception.ShiftNotPossibleException;
+
+@RunWith(JUnitPlatform.class)
+public class GearTransmissionTest {
+
+	private static final int MAXGEAR = 6;
+
+	private GearTransmission transmission;
+
+	@BeforeEach
+	public void setup() {
+		transmission = new GearTransmission(GearTransmissionTest.MAXGEAR);
+	}
+
+	@Test
+	@DisplayName("Wrong arguments for max gear")
+	void testConstructorWithWrongArgs() {
+		assertAll( //
+				() -> {
+					assertThrows(IllegalArgumentException.class, () -> new GearTransmission(-5));
+				}, //
+				() -> {
+					assertThrows(IllegalArgumentException.class, () -> new GearTransmission(0));
+				}, //
+				() -> {
+					new GearTransmission(1);
+				} //
+		);
+	}
+
+	@Test
+	@DisplayName("Initial state")
+	void testInitialState() {
+		assertThat(transmission.getCurrentGear(), is(nullValue()));
+		assertThat(transmission.getMaxGear(), is(equalTo(GearTransmissionTest.MAXGEAR)));
+	}
+
+	@Test
+	@DisplayName("Shifting up all available gears")
+	void testCurrentGear() throws ShiftNotPossibleException {
+		for (int i = 0; i < GearTransmissionTest.MAXGEAR; i++) {
+			transmission.shiftUp();
+			assertThat(transmission.getCurrentGear(), is(equalTo(i + 1)));
+		}
+	}
+
+	@Test
+	@DisplayName("Shifting up one gear too much")
+	void testCurrentGearException() throws ShiftNotPossibleException {
+		for (int i = 0; i < GearTransmissionTest.MAXGEAR; i++) {
+			transmission.shiftUp();
+		}
+		assertThrows(ShiftNotPossibleException.class, transmission::shiftUp);
+		// again, to test that the exception is thrown repeatedly
+		assertThrows(ShiftNotPossibleException.class, transmission::shiftUp);
+	}
+
+}
