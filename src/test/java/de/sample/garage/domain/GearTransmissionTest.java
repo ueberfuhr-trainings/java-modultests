f9e23dfc238b15b68907800ecd4c5891d41eb606
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-12-07 12:03:45 +0100

    Quick update

--- src/test/java/de/sample/garage/domain/GearTransmissionTest.java
+++ src/test/java/de/sample/garage/domain/GearTransmissionTest.java
@@ -3,10 +3,13 @@ package de.sample.garage.domain;
 import de.sample.garage.domain.exception.ShiftNotPossibleException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class GearTransmissionTest {
 
@@ -55,6 +58,19 @@ class GearTransmissionTest {
         // again, to test that the exception is thrown repeatedly
         assertThatThrownBy(transmission::shiftUp)
           .isInstanceOf(ShiftNotPossibleException.class);
+    }
+
+    @ParameterizedTest(name = "Shifting up with maximum gears of {0}")
+    @ValueSource(ints = {1,3,4,5,6,7,100})
+    void testShiftingParameterized(int maxGears) throws ShiftNotPossibleException {
+        GearTransmission gt = new GearTransmission(maxGears);
+        for (int i = 1; i <= maxGears; i++) {
+            gt.shiftUp();
+            assertThat(gt)
+              .extracting(GearTransmission::getCurrentGear)
+              .isEqualTo(i);
+        }
+        assertThrows(ShiftNotPossibleException.class, gt::shiftUp);
     }
 
 }
