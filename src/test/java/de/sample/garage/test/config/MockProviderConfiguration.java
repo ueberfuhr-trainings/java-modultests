0db63db7b031d40c692423ea1477fcf4fe65fe9e
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-12-12 13:15:50 +0100

    Switch to TestConfiguration

--- src/test/java/de/sample/garage/test/config/MockProviderConfiguration.java
+++ src/test/java/de/sample/garage/test/config/MockProviderConfiguration.java
@@ -1,11 +1,11 @@
 package de.sample.garage.test.config;
 
 import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Configuration
+@TestConfiguration
 @Profile("!spock")
 public class MockProviderConfiguration {
 
