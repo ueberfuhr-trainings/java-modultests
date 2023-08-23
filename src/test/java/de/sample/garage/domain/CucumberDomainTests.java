2253112c567e996733e5020ceaec338657cfe3f6
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-08-23 15:05:58 +0200

    Add hint for JDK 1.8

--- src/test/java/de/sample/garage/domain/CucumberDomainTests.java
+++ src/test/java/de/sample/garage/domain/CucumberDomainTests.java
@@ -1,10 +1,6 @@
 package de.sample.garage.domain;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.platform.suite.api.*;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
@@ -12,6 +8,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @IncludeEngines("cucumber")
 @SuiteDisplayName("Cucumber Tests")
 @SelectPackages({"de.sample.garage.domain", "features.domain"})
+@SelectClasspathResource("features.domain") // JDK 1.8
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "de.sample.garage.domain")
 public class CucumberDomainTests {
