3950ecd3c465b7e0da607f2bb1607f46f2865354
Ralf Ueberfuhr <ralf.ueberfuhr@yahoo.com>
2023-08-23 15:11:09 +0200

    Add hint for JDK 1.8

--- src/test/java/de/sample/garage/domain/CucumberDomainTests.java
+++ src/test/java/de/sample/garage/domain/CucumberDomainTests.java
@@ -4,6 +4,7 @@ import org.junit.platform.suite.api.*;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
 @Suite
 @IncludeEngines("cucumber")
 @SuiteDisplayName("Cucumber Tests")
