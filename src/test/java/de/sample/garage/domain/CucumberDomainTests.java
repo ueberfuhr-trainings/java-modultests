cd254508cf64078765a9a8df904f7d500d3afe07
Ralf Ueberfuhr <ralf.ueberfuhr@yahoo.com>
2023-08-23 15:14:18 +0200

    Add hint for JDK 1.8

--- src/test/java/de/sample/garage/domain/CucumberDomainTests.java
+++ src/test/java/de/sample/garage/domain/CucumberDomainTests.java
@@ -1,10 +1,6 @@
 package de.sample.garage.domain;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.platform.suite.api.*;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.OBJECT_FACTORY_PROPERTY_NAME;
